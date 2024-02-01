738fbf0a0d8494346172210d21d7e6e4e85bd92f
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-02-01 07:45:24 +0000

    updated routing system to better reflect auto-grading requirements

--- src/controllers/hw0.ts
+++ src/controllers/hw0.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express"
 
 async function get(req: Request, res: Response) {
+    res.setHeader("X-CSE356", process.env.SUBMISSION_ID)
     res.sendFile("hw0.html", { root: "html" })
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
+import hw0Controller from "@controllers/hw0"
 import rootController from "@controllers/index"
 import { Router } from "express"
 import hw0Router from "./hw0"
 
 const rootRouter = Router()
 rootRouter.get("/", rootController.get)
+rootRouter.use("/hw0.html", hw0Controller.get)
 rootRouter.use("/hw0", hw0Router)
 
 export default rootRouter
